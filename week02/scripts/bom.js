b7b47c6b853536a32db9065c7947db8db1fe3ecf
kamaludumullahele <kamalchandrakumar@gmail.comgit config --global user.email kamalchandrakumara@gmail.com>
2026-09-16 19:15:25 -0600

    bom files completion

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,16 +1,36 @@
 
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('______'); // fill in the blanks to refernce the unordered list element.
+const list = document.querySelector('ul'); // fill in the blanks to refernce the unordered list element.
 // Create a li element that will hold each entry's chapter title and an associated delete button.
-const li = document.createElement('li');
-// creating a delete button
-const deleteButton = document.createElement('button');
-//Populate the li element variable's textContent or innerHTML with the input value.
-li.textContent = input.value;
-// setting deleteButton textcontent to 
-deleteButton.textContent = '❌';
-//append deleteButton to li element
-li.append(deleteButton);
-// Append the li element variable to the unordered list in your HTML
-list.append(li);
+// const li = document.createElement('li');
+// // creating a delete button
+// const deleteButton = document.createElement('button');
+// //Populate the li element variable's textContent or innerHTML with the input value.
+// li.textContent = input.value;
+// // setting deleteButton textcontent to 
+// deleteButton.textContent = '❌';
+// //append deleteButton to li element
+// li.append(deleteButton);
+// // Append the li element variable to the unordered list in your HTML
+// list.append(li);
+//Create a click event listener for the Add Chapter button with addEventListener.
+button.addEventListener('click', function () {
+    if (input.value.trim() == "") {
+        return; // use return instead of break to exit the function
+    }
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        input.focus();
+    });
+    li.append(deleteButton);
+    list.append(li);
+    input.value = " "; // clear the input field after adding the chapter
+    input.focus(); // set focus back to the input field for convenience
+
+    
+});
